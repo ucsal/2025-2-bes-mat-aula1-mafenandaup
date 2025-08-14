e298282d826012afabdb65699588d351751ae131
Maria Fernanda <mafenandaup@gmail.com>
2025-08-14 20:45:07 -0300

    fix: polimorfismo dos métodos iguais

--- src/main/java/cms/user/AdminUser.java
+++ src/main/java/cms/user/AdminUser.java
@@ -2,35 +2,11 @@ package cms.user;
 
 public class AdminUser extends User {
 
-	public AdminUser(int id, String nome, String email, String password) {
-		super( id, nome, email, password);
-	}
-
-	@Override
-	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getNome() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getEmail() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean checkPassword(String p) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	 public AdminUser(int id, String nome, String email, String password) {
+	        super(id, nome, email, password);
+	    }
 
 	public void deleteContent(int contentId) {
-	/* exclusivo do admin */ }
+		/* exclusivo do admin */ }
 
 }
